233c22d97dbb696b3f8032603a57167d8386dbbc
njuante <juan.nunez1@alu.uclm.es>
2023-03-09 11:04:52 +0100

    Creacion clase empleado

---
+++ src/Ejer1/empleado.java
@@ -0,0 +1,20 @@
+package Ejer1;
+
+public class empleado{
+
+    public int getHoras() {
+        return 30; // trabaja 30 horas semanales
+    }
+
+    public double getSalario() {
+        return 30000.0; // 30,000.00 / año
+    }
+
+    public int getVacaciones() {
+        return 10; // 2 semanas
+    }
+
+    public String getMesVacaciones() {
+        return "Agosto"; } // mes de vacaciones
+
+}
